1fd9cce75463b12f440374d8368f2a082cb9bc9b
rathalye47 <65984956+rathalye47@users.noreply.github.com>
2021-12-28 16:10:31 -0800

    Add files via upload

---
+++ Homework/Homework1/homework1.R
@@ -0,0 +1,14 @@
+# Rohan Athalye
+# Homework 1: 1.5 Exercises
+
+# 1.(a) R version 4.1.1
+# 1.(b) Platform: x86_64-w64-mingw32/x64 (64-bit)
+# 1.(c) demo(), help(), help.start(), q()
+
+# 2.(a) Ctrl+O
+# 2.(b) Ctrl+L
+
+# 3.(a) [1] 1
+# 3.(b) [1] 1. The result corresponds to the second line with the command "print()". The result of the first line was not produced.
+
+
